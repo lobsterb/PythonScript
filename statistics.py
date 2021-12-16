746747101f86952b0fe72e0bbb3149c154b01089
DESKTOP-J0QDQQL\KoKo <57263573@qq.com>
2021-12-16 11:43:37 +0800

    调整代码, 以URL作为统计标准

--- statistics.py
+++ statistics.py
@@ -9,12 +9,12 @@ import time
 import datetime
 
 gHistory = None
+gLimit = 500000
 
 
-class RequestInfo:
+class IpInfo:
+
     def __init__(self):
-        # 请求头
-        self.header_ = ""
         # ua <str, int>
         self.dictUa = {}
         # 请求返回状态 <str, int>
@@ -37,15 +37,19 @@ class RequestInfo:
         self.firstReqTime_ = ""
         # 最后一个请求时间 = ""
         self.lastReqTime_ = ""
+        # 访问次数
+        self.requestCnts_ = 0
 
 
-class HostInfo:
+class RequestInfo:
     def __init__(self):
         self.host_ = ""
-        # <reqHeader, RequestInfo>
-        self.dictRequests_ = {}
-        # 请求类别总数
-        self.totalRequests_ = 0
+        # 请求头
+        self.header_ = ""
+        # ip地址 <str, IpInfo>
+        self.dictIp_ = {}
+        # 总计访问次数
+        self.requestCnts_ = 0
 
 
 class HandleHistory:
@@ -54,10 +58,12 @@ class HandleHistory:
         self.dictHandleFiles_ = {}
         self.totalProgressTime_ = 0
         self.handleCnt_ = 0
-        # 所有host <ip, host>
-        self.dictHost_ = {}
-        # host类别总数
-        self.totalHost_ = 0
+        # 所有host <requestHeader, Request>
+        self.dictRequest_ = {}
+        self.dictHR_ = {}
+        self.dictIp_ = {}
+        self.startTime_ = time.time()
+        self.handleSize_ = 0.0
 
 
 def saveProgress():
@@ -112,106 +118,109 @@ def convertTime(unixTime):
     t = int(unixTime / 1000)
     t = datetime.datetime.fromtimestamp(t)
     strTime = t.strftime("%Y-%m-%d %H:%M:%S")
-    return strTime
+    return strTime.strip()
 
 
 def readOneRow(row):
     field = Field()
-    field.__FILENAME__ = row[0]
-    field.__SOURCE__ = row[1]
-    s = row[2]
-    if s.startswith(("__")):
+    try:
+        field.__FILENAME__ = row[0].strip()
+        field.__SOURCE__ = row[1].strip()
+        field.__TIMESTAMP__ = float(row[2])
+        field.localTime_ = convertTime(field.__TIMESTAMP__)
+        field.__TOPICID__ = row[3].strip()
+        field.__CONTENT__ = row[4].strip()
+        field.LogParseFailure = row[5].strip()
+        field.__TAG__container_name = row[6].strip()
+        field.__TAG__namespace = row[7].strip()
+        field.__TAG__pod_name = row[8].strip()
+        field.host = row[9].strip()
+        field.http_user_agent = row[10].strip()
+        field.http_x_forwarded_for = row[11].strip()
+        field.http_x_vlight_market = row[12].strip()
+        field.http_x_vlight_os_name = row[13].strip()
+        field.http_x_vlight_udid = row[14].strip()
+        field.http_x_vlight_user_id = row[15].strip()
+        field.http_x_vlight_version = row[16].strip()
+        field.remote_addr = row[17].strip()
+        field.request = row[18].strip()
+        field.request_body = row[19].strip()
+        field.request_time = row[20].strip()
+        field.status = row[21].strip()
+        field.upstream_status = row[22].strip()
+    except:
         return None
-    field.__TIMESTAMP__ = float(float(row[2]))
-    field.localTime_ = convertTime(field.__TIMESTAMP__)
-    field.__TOPICID__ = row[3]
-    field.__CONTENT__ = row[4]
-    field.LogParseFailure = row[5]
-    field.__TAG__container_name = row[6]
-    field.__TAG__namespace = row[7]
-    field.__TAG__pod_name = row[8]
-    field.host = row[9]
-    field.http_user_agent = row[10]
-    field.http_x_forwarded_for = row[11]
-    field.http_x_vlight_market = row[12]
-    field.http_x_vlight_os_name = row[13]
-    field.http_x_vlight_udid = row[14]
-    field.http_x_vlight_user_id = row[15]
-    field.http_x_vlight_version = row[16]
-    field.remote_addr = row[17]
-    field.request = row[18]
-    field.request_body = row[19]
-    field.request_time = row[20]
-    field.status = row[21]
-    field.upstream_status = row[22]
+
     return field
 
 
-def createRequestInfo(filed):
-    ri = RequestInfo()
-    ri.header_ = filed.request
-    ri.dictUa[filed.http_user_agent] = 1
-    ri.dictStatus_[filed.status] = 1
-    ri.dictUpstreamStatus_[filed.upstream_status] = 1
-    ri.dictAppVersion_[filed.http_x_vlight_version] = 1
-    ri.dictUserId_[filed.http_x_vlight_user_id] = 1
-    ri.dictOsName_[filed.http_x_vlight_os_name] = 1
-    ri.dictUdid_[filed.http_x_vlight_udid] = 1
-    ri.dictMarket[filed.http_x_vlight_market] = 1
-    ri.requestTotalTime_ = filed.request_time
-    ri.firstReqTime_ = filed.__TIMESTAMP__
-    ri.lastReqTime_ = filed.__TIMESTAMP__
-    return ri
+def createIpInfo(filed):
+    ip = IpInfo()
+    ip.dictUa[filed.http_user_agent] = 1
+    ip.dictStatus_[filed.status] = 1
+    ip.dictUpstreamStatus_[filed.upstream_status] = 1
+    ip.dictAppVersion_[filed.http_x_vlight_version] = 1
+    ip.dictUserId_[filed.http_x_vlight_user_id] = 1
+    ip.dictOsName_[filed.http_x_vlight_os_name] = 1
+    ip.dictUdid_[filed.http_x_vlight_udid] = 1
+    ip.dictMarket[filed.http_x_vlight_market] = 1
+    ip.requestTotalTime_ = filed.request_time
+    ip.firstReqTime_ = filed.__TIMESTAMP__
+    ip.lastReqTime_ = filed.__TIMESTAMP__
+    ip.requestCnts_ += 1
+    return ip
 
 
-def updateRequestInfo(ri, filed):
-    if filed.http_user_agent in ri.dictUa:
-        ri.dictUa[filed.http_user_agent] += 1
+def updateIpInfo(ip, filed):
+    if filed.http_user_agent in ip.dictUa:
+        ip.dictUa[filed.http_user_agent] += 1
     else:
-        ri.dictUa[filed.http_user_agent] = 1
+        ip.dictUa[filed.http_user_agent] = 1
 
-    if filed.status in ri.dictStatus_:
-        ri.dictStatus_[filed.status] += 1
+    if filed.status in ip.dictStatus_:
+        ip.dictStatus_[filed.status] += 1
     else:
-        ri.dictStatus_[filed.status] = 1
+        ip.dictStatus_[filed.status] = 1
 
-    if filed.upstream_status in ri.dictUpstreamStatus_:
-        ri.dictUpstreamStatus_[filed.upstream_status] += 1
+    if filed.upstream_status in ip.dictUpstreamStatus_:
+        ip.dictUpstreamStatus_[filed.upstream_status] += 1
     else:
-        ri.dictUpstreamStatus_[filed.upstream_status] = 1
+        ip.dictUpstreamStatus_[filed.upstream_status] = 1
 
-    if filed.http_x_vlight_version in ri.dictAppVersion_:
-        ri.dictAppVersion_[filed.http_x_vlight_version] += 1
+    if filed.http_x_vlight_version in ip.dictAppVersion_:
+        ip.dictAppVersion_[filed.http_x_vlight_version] += 1
     else:
-        ri.dictAppVersion_[filed.http_x_vlight_version] = 1
+        ip.dictAppVersion_[filed.http_x_vlight_version] = 1
 
-    if filed.http_x_vlight_user_id in ri.dictUserId_:
-        ri.dictUserId_[filed.http_x_vlight_user_id] += 1
+    if filed.http_x_vlight_user_id in ip.dictUserId_:
+        ip.dictUserId_[filed.http_x_vlight_user_id] += 1
     else:
-        ri.dictUserId_[filed.http_x_vlight_user_id] = 1
+        ip.dictUserId_[filed.http_x_vlight_user_id] = 1
 
-    if filed.http_x_vlight_os_name in ri.dictOsName_:
-        ri.dictOsName_[filed.http_x_vlight_os_name] += 1
+    if filed.http_x_vlight_os_name in ip.dictOsName_:
+        ip.dictOsName_[filed.http_x_vlight_os_name] += 1
     else:
-        ri.dictOsName_[filed.http_x_vlight_os_name] = 1
+        ip.dictOsName_[filed.http_x_vlight_os_name] = 1
 
-    if filed.http_x_vlight_udid in ri.dictUdid_:
-        ri.dictUdid_[filed.http_x_vlight_udid] += 1
+    if filed.http_x_vlight_udid in ip.dictUdid_:
+        ip.dictUdid_[filed.http_x_vlight_udid] += 1
     else:
-        ri.dictUdid_[filed.http_x_vlight_udid] = 1
+        ip.dictUdid_[filed.http_x_vlight_udid] = 1
 
-    if filed.http_x_vlight_market in ri.dictMarket:
-        ri.dictMarket[filed.http_x_vlight_market] += 1
+    if filed.http_x_vlight_market in ip.dictMarket:
+        ip.dictMarket[filed.http_x_vlight_market] += 1
     else:
-        ri.dictMarket[filed.http_x_vlight_market] = 1
+        ip.dictMarket[filed.http_x_vlight_market] = 1
 
-    ri.requestTotalTime_ += filed.request_time
+    ip.requestTotalTime_ += filed.request_time
 
-    if filed.__TIMESTAMP__ < ri.firstReqTime_:
-        ri.firstReqTime_ = filed.__TIMESTAMP__
+    if filed.__TIMESTAMP__ < ip.firstReqTime_:
+        ip.firstReqTime_ = filed.__TIMESTAMP__
 
-    if filed.__TIMESTAMP__ > ri.lastReqTime_:
-        ri.lastReqTime_ = filed.__TIMESTAMP__
+    if filed.__TIMESTAMP__ > ip.lastReqTime_:
+        ip.lastReqTime_ = filed.__TIMESTAMP__
+
+    ip.requestCnts_ += 1
 
 
 def extratHeader(request):
@@ -243,7 +252,7 @@ def extratHeader(request):
     for p in vecPeaces:
         p = p.strip()
 
-        if "?" in p or "." in p:
+        if "?" in p or "." in p or "%" in p or '-' in p:
             # 参数部分
             break
 
@@ -251,17 +260,18 @@ def extratHeader(request):
             fullHeader += "/"
         else:
             # 判断有没有可能是md5
-            if len(p) == 32:
-                isMd5 = True
-                for c in p:
-                    if not (('0' <= c <= '9') or ('a' <= c <= 'f') or ('A' <= c <= 'F')):
-                        isMd5 = False
-                        break
-
-                if isMd5:
-                    fullHeader += "xxx/"
-                else:
-                    fullHeader += p + "/"
+            if len(p) == 32 or len(p) == 24 or len(p) == 20:
+                fullHeader += "xxx/"
+                # isMd5 = True
+                # for c in p:
+                #     if not (('0' <= c <= '9') or ('a' <= c <= 'f') or ('A' <= c <= 'F')):
+                #         isMd5 = False
+                #         break
+                #
+                # if isMd5:
+                #     fullHeader += "xxx/"
+                # else:
+                #     fullHeader += p + "/"
             else:
                 # 检测是否是纯数字
                 isNumber = True
@@ -278,8 +288,21 @@ def extratHeader(request):
     return fullHeader
 
 
+def newRequest(fullHeader, ip, filed):
+    global gHistory
+    ri = RequestInfo()
+    ri.host_ = filed.host
+    ri.header_ = fullHeader
+    ri.dictIp_[filed.remote_addr] = ip
+    gHistory.dictRequest_[fullHeader] = ri
+    ri.requestCnts_ += 1
+    gHistory.dictHR_[fullHeader] = filed.host
+    return ri
+
+
 def readCsv(fullPath):
     global gHistory
+    global gLimit
 
     # 判断文件是否处理过, 可能因为上次异常中断
     if fullPath in gHistory.dictHandleFiles_:
@@ -287,57 +310,72 @@ def readCsv(fullPath):
         logger.warning("已经处理过, 上次处理耗时:{}, 文件:{}, ", useTime, fullPath)
         return
 
-    logger.info("开始处理文件: {}, 大小:{}", fullPath, os.path.getsize(fullPath) / 1024 / 1024)
+    fileSize = round(os.path.getsize(fullPath) / 1024 / 1024 / 1024, 2)
+    logger.info("开始处理文件: {}, 大小:{}GB", fullPath, fileSize)
     fileCnt_ = 0
     startTime = time.time()
     with open(fullPath, 'r', newline='', encoding='utf8') as f:
         reader = csv.reader(f)
+
+        fileHandleCnt = 0
+        limitStartTime = time.time()
+
         for row in reader:
+
+            if row[0].startswith("__"):
+                continue
+
             filed = readOneRow(row)
             if filed is None:
                 continue
-            gHistory.handleCnt_ += 1
-            fileCnt_ += 1
 
-            fullHeader = extratHeader(filed.request)
+            fullHeader = extratHeader(filed.request).strip()
             if fullHeader == "":
                 continue
 
-            # filed.remote_addr = "127.0.0.1"
+            ip = createIpInfo(filed)
 
-            # 查找该IP是否存在访问记录
-            if filed.remote_addr in gHistory.dictHost_:
-                hostInfo = gHistory.dictHost_[filed.remote_addr]
-                # 判断该请求是否存在
-                if fullHeader in hostInfo.dictRequests_:
-                    ri = hostInfo.dictRequests_[fullHeader]
-                    updateRequestInfo(ri, filed)
+            # 查找该请求是否存在
+            if fullHeader in gHistory.dictRequest_:
+                ri = gHistory.dictRequest_[fullHeader]
+                # 判断ip是否存在
+                if filed.remote_addr in ri.dictIp_:
+                    ip = ri.dictIp_[filed.remote_addr]
+                    updateIpInfo(ip, filed)
+                    ri.requestCnts_ += 1
                 else:
-                    ri = createRequestInfo(filed)
-                    hostInfo.dictRequests_[fullHeader] = ri
-                hostInfo.totalRequests_ += 1
+                    ri = newRequest(fullHeader, ip, filed)
             else:
-                # 创建一个新的ip访问记录
-                hostInfo = HostInfo()
-                hostInfo.host_ = filed.host
-                ri = createRequestInfo(filed)
-                hostInfo.dictRequests_[fullHeader] = ri
-                gHistory.dictHost_[filed.remote_addr] = hostInfo
-                hostInfo.totalRequests_ += 1
+                ri = newRequest(fullHeader, ip, filed)
 
-            # logger.info("ip:{}, host:{}, req:{}, 处理数据:{}条", filed.remote_addr, filed.host, fullHeader,
-            #             hostInfo.totalRequests_)
+            gHistory.handleCnt_ += 1
+            fileCnt_ += 1
+            fileHandleCnt += 1
+            ip = filed.remote_addr.strip()
 
-    endTime = time.time()
+            if ip in gHistory.dictIp_:
+                gHistory.dictIp_[ip] += 1
+            else:
+                gHistory.dictIp_[ip] = 1
 
-    useTime = endTime - startTime
-    logger.info("处理文件:{}, 耗时:{}, 该文件处理条数:{}, 总条数:{}", fullPath, useTime, fileCnt_, gHistory.handleCnt_)
+            if fileHandleCnt >= gLimit:
+                limitUseTime = time.time() - limitStartTime
+                logger.info("处理:{}条数据, 单次耗时:{}, url分类总个数:{}, 访问ip总个数:{}, 总处理条数:{}", fileHandleCnt,
+                            round(limitUseTime, 2),
+                            len(gHistory.dictRequest_), len(gHistory.dictIp_), gHistory.handleCnt_)
+                fileHandleCnt = 0
+                limitStartTime = time.time()
+
+    gHistory.handleSize_ += fileSize
+    useTime = time.time() - startTime
+    logger.warning("处理文件:{}, 文件总计耗时:{}, 该文件处理条数:{}, 总处理条数:{}, 总处理文件大小:{}GB", fullPath, round(useTime, 2), fileCnt_,
+                gHistory.handleCnt_,
+                round(gHistory.handleSize_, 2))
 
     gHistory.totalProgressTime_ += useTime
     gHistory.dictHandleFiles_[fullPath] = useTime
 
     saveProgress()
-    exit(0)
 
 
 def TraverseDir(rootDir):
@@ -356,6 +394,7 @@ def initLog():
 
 
 def main():
+    global gHistory
     initLog()
     logger.info(gConfig)
 
@@ -371,7 +410,7 @@ def main():
     endTime = time.time()
     useTime = endTime - startTime
 
-    logger.info("程序结束, 总共耗时:{}", useTime)
+    logger.error("程序结束, 单次运行耗时:{}, 总计运行耗时:{}", round(useTime, 2), round(time.time() - gHistory.startTime_, 2))
 
 
 if __name__ == '__main__':
